e2f8b6a873919015e11e4fce9341b2d5b2b019e2
Jefferson Stinson <jefferson@stinson.dev>
2024-09-10 19:49:42 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
 const menuBtn = document.querySelector(".menu_button");
 const menuItems = document.querySelector(".menu__items");
 const expandBtn = document.querySelectorAll(".expand-btn");
+const menuBtns = document.getElementById("mobile_nav").getElementsByClassName("header_link");
 
 // nav toggle
 menuBtn.addEventListener("click", () =>{
+    menuBtn.classList.toggle("open");
+    menuItems.classList.toggle("open");
+});
+
+menuBtns.addEventListener("click", () =>{
     menuBtn.classList.toggle("open");
     menuItems.classList.toggle("open");
 });
