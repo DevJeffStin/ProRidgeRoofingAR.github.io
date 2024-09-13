0022759f62795a954a1c4a2ecd7fab860bdbfcd5
Jefferson Stinson <jefferson@stinson.dev>
2024-09-12 20:27:36 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,12 +18,11 @@ expandBtn.forEach((btn) => {
 });
 
 for (var i = 0; i < menuBtns.length; i ++) {
-    onclick = function() {
+    menuBtns[i].onclick = function() {
         if(menuBtn.classList.contains("open")){
             menuBtn.classList.remove("open");
             menuItems.classList.remove("open");
             console.log("click");
         }
     }
-    
 }
